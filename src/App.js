2dcc9811bd3c375997e036dc2fdccf86c1a72ec7
Breon Knight <Breonknight7@gmail.com>
2017-04-28 09:50:19 -0700

    fixed materalize issues

--- src/App.js
+++ src/App.js
@@ -1,20 +1,31 @@
 import React, { Component } from 'react';
+import { Button, Card, Row, Col, Icon } from 'react-materialize';
 import logo from './logo.svg';
 import './App.css';
+
 
 class Wow extends Component {
   render() {
     return (
-      <div>
-        <p>lez</p>
-        <h1>get it</h1>
-      </div>
+      <Row>
+        <Col s={1} className='grid-example'>1</Col>
+        <Col s={1} className='grid-example'>2</Col>
+        <Col s={1} className='grid-example'>3</Col>
+        <Col s={1} className='grid-example'>4</Col>
+        <Col s={1} className='grid-example'>5</Col>
+        <Col s={1} className='grid-example'>6</Col>
+        <Col s={1} className='grid-example'>7</Col>
+        <Col s={1} className='grid-example'>8</Col>
+        <Col s={1} className='grid-example'>9</Col>
+        <Col s={1} className='grid-example'>10</Col>
+        <Col s={1} className='grid-example'>11</Col>
+        <Col s={1} className='grid-example'>12</Col>
+      </Row>
     )
   }
 }
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
